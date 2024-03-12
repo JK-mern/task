291df5c7a039cd8580922669f5e3ed7b3765dca1
jkmerndev <jkmerndev@gmail.com>
2024-03-12 22:54:27 +0530

    created a model for Products

---
+++ server/models/Product.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose"
+
+const productSchema= new  mongoose.Schema({
+    name : {
+        type : String,
+        required : true,
+        unique : true,
+    },
+    description : {
+        type : String,
+        required : true,
+    },
+    featured : {
+        type : Boolean,
+        required : true
+    },
+    image : {
+        type : String,
+        required : true
+    },
+
+},{timestamps : true})
+
+const Product =  mongoose.model("Product", productSchema)
+export default Product
