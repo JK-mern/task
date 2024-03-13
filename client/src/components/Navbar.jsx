f9b1003fe0d594b984940495fc615097865941e2
jkmerndev <jkmerndev@gmail.com>
2024-03-13 23:47:15 +0530

    updated navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React from "react";
 
 function Navbar() {
+  const handleLogout = async () => {};
   return (
-    <nav className='bg-gray-900'>
-        <div className='flex max-w-screen-2xl font-serif justify-between  py-5 items-center    mx-auto lg:flex-row'>
-            <div >
-                <h3 className='text-3xl ml-5 md:ml-0 font-bold cursor-pointer justify-center text-center text-white '>Shopify</h3>
-            </div>
-            <div>
-                <button className='mr-6 font-mono md:mr-0  btn  bg-white text-black hover:text-white'>Logout</button>
-            </div>
+    <nav className="bg-gray-900">
+      <div className="flex max-w-screen-2xl font-serif justify-between  py-5 items-center    mx-auto lg:flex-row">
+        <div>
+          <h3 className="text-3xl ml-5 md:ml-4 font-bold cursor-pointer justify-center text-center text-white ">
+            Shopify
+          </h3>
         </div>
+        <div>
+          <button
+            onClick={handleLogout}
+            className="mr-6 font-mono md:mr-0  btn  bg-white text-black hover:text-white"
+          >
+            Logout
+          </button>
+        </div>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
