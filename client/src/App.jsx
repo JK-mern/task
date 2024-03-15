37cfc455bc0434327f1681e78c1e87396339b4df
jkmerndev <jkmerndev@gmail.com>
2024-03-15 22:58:56 +0530

    made /home a private route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,26 @@
-
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import SignUp from "./Pages/SignUp"
-import SignIn from "./Pages/SignIn"
-import { useSelector } from "react-redux"
-import Home from "./Pages/Home"
-import AddProducts from "./Pages/AddProducts"
-function App  (){
-  const  {currentUser } = useSelector ((state) => state.user)
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUp from "./Pages/SignUp";
+import SignIn from "./Pages/SignIn";
+import { useSelector } from "react-redux";
+import Home from "./Pages/Home";
+import AddProducts from "./Pages/AddProducts";
+import PrivateRoute from "./components/PrivateRoute";
+function App() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
-    <BrowserRouter >
+    <BrowserRouter>
       <Routes>
-        <Route  path="/"  element={<SignUp/>} />
-        <Route  path="/signin"  element={<SignIn/>} />
-        <Route path="/home" element={currentUser?.seller ? <AddProducts/> : <Home/>} />
+        <Route path="/" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route element={<PrivateRoute />}>
+          <Route
+            path="/home"
+            element={currentUser?.seller ? <AddProducts /> : <Home />}
+          ></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App 
+export default App;
